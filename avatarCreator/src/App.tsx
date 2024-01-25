b44caf5350fe63a32aa9ee88f7ccef9c4b54fcf2
Christopher Bucher <bucher.christopher@gmail.com>
2024-01-25 09:30:41 +0100

    create api folder to store server adress, create first random avatar on the landing page

--- avatarCreator/src/App.tsx
+++ avatarCreator/src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import { SERVER } from '../api/fetchAvatar'
 
 function App() {
+  const [hash, setHash] = useState("random"); 
 
   return (
     <>
-    test
+    <div>
+      this is a random avatar
+    </div>
+    <img src={SERVER + hash} alt="random Avatar" />
     </>
   )
 }
